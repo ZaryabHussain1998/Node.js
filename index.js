2e617fcaee82523d3d2f2a910279b66325b58dd6
ZaryabHussain1998 <zaryabhussain72@gmail.com>
2022-06-07 09:20:02 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+/*
+//import export and filter function
+const app = require('./app')
+console.log(app.z())
+const arr=[1,2,3,4,4,455,45,344]
+let result =arr.filter((item)=>{
+    return item<=4
+})
+console.warn(result)
+
+
+//core Modules
+const fs = require('fs').writeFileSync;
+console.log(__filename)
+fs("hello3.txt","i am here")
+
+//create server
+const http = require('http');
+http.createServer((req,resp)=>{
+    resp.write("<h1>Hello this is Zaryab Hussain")
+    resp.end();
+}).listen(4500);
+
+//difference between regular function and arrow
+   function regular(a)
+   {
+       return a*10;
+   }
+   
+   console.log(regular(5));
+   const arrowpure = (a)=>a*4;
+   console.log(arrowpure(5));*/
+   console.log("package.json")
+
+   
