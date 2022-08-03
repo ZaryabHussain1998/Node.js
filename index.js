49457baff3c67abf718d82846b4caaed5146d67c
ZaryabHussain1998 <zaryabhussain72@gmail.com>
2022-08-03 04:01:57 -0700

    upload

--- index.js
+++ index.js
@@ -604,30 +604,92 @@ app.get("/search/:key", async (req, resp) => {
 app.listen(5000);*/
 
 
-// const express = require('express');
-// const multer = require('multer');
+const express = require('express');
+const multer = require('multer');
 
+const app = express();
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            console.log("destination is working")
+            cb(null, 'uploads')
+        },
+        filename: function (req, file, cb) {
+            console.log("filename is working")
+            cb(null, file.fieldname+ "-" +Date.now()+ ".txt")
+        } 
+    })
+});
+
+app.post("/upload", upload.single('file_name'), (req, resp) => {
+    resp.send("file uploaded")
+})
+
+app.listen(5000);
+
+// const express = require("express");
 // const app = express();
+// const db = require("./models");
+// const { Users } = require("./models");
 
-// const upload = multer({
-//     storage: multer.diskStorage({
-//         destination: function (req, file, cb) {
-//             console.log("destination is working")
-//             cb(null, 'uploads')
-//         },
-//         filename: function (req, file, cb) {
-//             console.log("filename is working")
-//             cb(null, file.fieldname+ "-" +Date.now()+ ".txt")
-//         } 
+// const bcrypt = require("bcrypt");
+// const cookieParser = require("cookie-parser");
+// const { createTokens, validateToken } = require("./JWT");
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.post("/register", (req, res) => {
+//   const { username, password } = req.body;
+//   bcrypt.hash(password, 10).then((hash) => {
+//     Users.create({
+//       username: username,
+//       password: hash,
 //     })
+//       .then(() => {
+//         res.json("USER REGISTERED");
+//       })
+//       .catch((err) => {
+//         if (err) {
+//           res.status(400).json({ error: err });
+//         }
+//       });
+//   });
 // });
 
-// app.post("/upload", upload.single('file_name'), (req, resp) => {
-//     resp.send("file uploaded")
-// })
+// app.post("/login", async (req, res) => {
+//   const { username, password } = req.body;
 
-// app.listen(5000);
+//   const user = await Users.findOne({ where: { username: username } });
 
-var acl = require('acl');
-acl = new acl(new acl.mongodbBackend());
-acl.allow('guest', 'blogs', 'view')
+//   if (!user) res.status(400).json({ error: "User Doesn't Exist" });
+
+//   const dbPassword = user.password;
+//   bcrypt.compare(password, dbPassword).then((match) => {
+//     if (!match) {
+//       res
+//         .status(400)
+//         .json({ error: "Wrong Username and Password Combination!" });
+//     } else {
+//       const accessToken = createTokens(user);
+
+//       res.cookie("access-token", accessToken, {
+//         maxAge: 60 * 60 * 24 * 30 * 1000,
+//         httpOnly: true,
+//       });
+
+//       res.json("LOGGED IN");
+//     }
+//   });
+// });
+
+// app.get("/profile", validateToken, (req, res) => {
+//   res.json("profile");
+// });
+
+// db.sequelize.sync().then(() => {
+//   app.listen(3001, () => {
+//     console.log("SERVER RUNNING ON PORT 3001");
+//   });
+// });
